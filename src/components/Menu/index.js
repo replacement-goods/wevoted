6504d749e927e3708db28c043a3427408a90a100
Andy Gillette <andy.gillette@gmail.com>
2019-07-01 16:56:03 -0700

    hamburger stub

---
+++ src/components/Menu/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './menu.module.scss';
+
+const Menu = () => {
+  return (
+    <a className={styles.navToggle} href="#"><span></span></a>
+  );
+};
+
+export default Menu;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import { Link } from 'gatsby';
-
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import Intro from '../components/Intro';
-import Press from '../components/Press';
 import Problem from '../components/Problem';
 import CallToAction from '../components/CallToAction';
 import Solution from '../components/Solution';
 import Products from '../components/Products';
 import Subscription from '../components/Subscription';
 import About from '../components/About';
+import Menu from '../components/Menu';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="We Voted to Unseat Terrible Right Wind Politicians" />
+    <Menu />
     <Intro />
     <Problem />
     <Solution />
