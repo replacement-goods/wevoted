d7f83fbc9e7202890594de7dc09f9ee40cbc5c64
Andy Gillette <andy.gillette@gmail.com>
2019-07-09 11:33:59 -0700

    new subscription section

--- src/components/Subscription/index.js
+++ src/components/Subscription/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './subscription.module.scss';
 import InnerContainer from '../../elements/InnerContainer';
 import subscription from '../../images/subscription.png';
+import theBox from '../../images/theBox.png';
 
 const Option = ({ size, items, cost }) => (
   <div className={styles.option}>
@@ -16,9 +17,12 @@ const Option = ({ size, items, cost }) => (
 const Subscription = () => {
   return (
     <div className={styles.subscriptionContainer}>
-      <div className={styles.pinkBackground}>
+      <div className={styles.boxImage}>
+        <img src={theBox} />
+      </div>
+      <div className={styles.greenBackground}>
         <InnerContainer>
-          <h3 className={styles.darkPinkText}>When we launch in the fall we'll be offering our products in monthly subscription boxes.</h3>
+          <h3 className={styles.darkGreenText}>When we launch in the fall we'll be offering our products in monthly subscription boxes.</h3>
           <h3 className={styles.whiteText}>By subscribing you are taking direct action to throw the bastards out.</h3>
         </InnerContainer>
       </div>
