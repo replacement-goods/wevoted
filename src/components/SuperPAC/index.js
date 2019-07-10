2f854d9f766c4ff7bd5023475cdc527d2d43df9c
Andy Gillette <andy.gillette@gmail.com>
2019-07-10 16:56:59 -0700

    super pac seal image

--- src/components/SuperPAC/index.js
+++ src/components/SuperPAC/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import InnerContainer from '../../elements/InnerContainer';
 import styles from './superpac.module.scss';
+import pacSeal from '../../images/pacSeal.png';
 
 const SuperPAC = () => {
   return (
     <div className={styles.superPacContainer}>
       <InnerContainer>
         <div className={styles.whiteBackground}>
-        <p>The We Voted Super PAC is a wholly independent entity from We Voted (which is owned by Replacement Goods Inc.).</p>
-        <p>The We Voted Super PAC was formed in July 2019 with the following objectives:</p>
-        <p>To oppose candidates at all levels of government who are not actively working to:</p>
-        <p className={styles.bold}>…enact aggressive climate change legislation guided by science.</p>
-        <p className={styles.bold}>…protect women’s right to choose and get government out of women’s reproductive issues.</p>
-        <p className={styles.bold}>…reform gun laws to help prevent the scourge of gun violence in our society.</p>
-        <p className={styles.bold}>…change our immigration laws to treat migrants humanely and give Dreamers permanent legal status.</p>
-        <p className={styles.bold}>…ensure that high quality affordable universal healthcare is a human right ensured to every person in this country.</p>
+          <img src={pacSeal} className={styles.pacImage} />
+          <p>The We Voted Super PAC is a wholly independent entity from We Voted (which is owned by Replacement Goods Inc.).</p>
+          <p>The We Voted Super PAC was formed in July 2019 with the following objectives:</p>
+          <p>To oppose candidates at all levels of government who are not actively working to:</p>
+          <p className={styles.bold}>…enact aggressive climate change legislation guided by science.</p>
+          <p className={styles.bold}>…protect women’s right to choose and get government out of women’s reproductive issues.</p>
+          <p className={styles.bold}>…reform gun laws to help prevent the scourge of gun violence in our society.</p>
+          <p className={styles.bold}>…change our immigration laws to treat migrants humanely and give Dreamers permanent legal status.</p>
+          <p className={styles.bold}>…ensure that high quality affordable universal healthcare is a human right ensured to every person in this country.</p>
         </div>
       </InnerContainer>
     </div>
