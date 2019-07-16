1f6d65ba63e680e179e2d4cf6e3ecda6349ff516
Andy Gillette <andy.gillette@gmail.com>
2019-07-15 18:24:07 -0700

    basics section wip

--- src/components/Subscription/index.js
+++ src/components/Subscription/index.js
@@ -3,35 +3,22 @@ import styles from './subscription.module.scss';
 import InnerContainer from '../../elements/InnerContainer';
 import subscription from '../../images/subscription.png';
 import theBox from '../../images/theBox.png';
+import HalfPageBlock from '../../elements/HalfPageBlock';
 
 const Subscription = () => {
   return (
     <div className={styles.subscriptionContainer}>
-      <div className={styles.boxImage}>
+      <HalfPageBlock className={styles.boxImage}>
         <img src={theBox} />
-      </div>
-      <div className={styles.greenBackground}>
+      </HalfPageBlock>
+      <HalfPageBlock className={styles.basics}>
         <InnerContainer>
-          <h3 className={styles.whiteText}>This is our basic box<br />$24.99 per month.</h3>
-          <h3 className={styles.darkGreenText}>It contains hand soap, dish soap, laundry detergent pods and kitchen garbage bags.</h3>
-          <h3 className={styles.whiteText}><a href="https://www.indiegogo.com/projects/these-home-goods-defeat-right-wing-politicians/" target="_blank">You can order it now from our IndieGoGo campaign.</a></h3>
+          <h3>The Basics</h3>
+          <a href="https://www.indiegogo.com/projects/these-home-goods-defeat-right-wing-politicians/" target="_blank">(Available now via our IndieGoGO campaign)</a>
+          <h4>$24.99 / Month</h4>
+          <p>It contains hand soap, dish soap, laundry detergent pods and kitchen garbage bags.</p>
         </InnerContainer>
-      </div>
-      <svg
-        className={styles.edge}
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1920 20"
-        preserveAspectRatio="none"
-      >
-        <path d="M-.5,19.5V-.5h1920v20s-435-16-960-16S-.5,19.5-.5,19.5Z" transform="translate(0.5 0.5)"/>
-      </svg>
-      <div className={styles.subscriptionImage}>
-        <img src={subscription} />
-      </div>
-      <InnerContainer>
-        <h3 className={styles.blueText}>We get that this seems kinda crazy and we can imagine that you’re skeptical.</h3>
-        <h3 className={styles.blackBlueText}>Why should you trust that the political action that we are going to support will be in line with your values?</h3>
-      </InnerContainer>
+      </HalfPageBlock>
     </div>
   );
 };
