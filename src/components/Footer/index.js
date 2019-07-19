d919ad3e3c024319bd2f541c1cd7d6da5a3738c7
Andy Gillette <andy.gillette@gmail.com>
2019-07-18 21:10:17 -0700

    desktop footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,26 +11,28 @@ const Footer = () => {
   return (
     <footer className={styles.mainFooter}>
       <InnerContainer>
-        <form className={styles.newsletterForm}>
-          <label for="email">Join our mailing list</label>
-          <input type="email" name="email" placeholder="Enter your email address" />
-          <button type="submit">
-            <img src={button} />
-          </button>
-        </form>
-        <div className={styles.socialLinks}>
-          <a href="mailto:contact@wevoted.com">
-            <img src={email} />
-          </a>
-          <a href="https://www.facebook.com/wevotedofficial" target="_blank">
-            <img src={facebook} />
-          </a>
-          <a href="https://www.twitter.com/wevotedofficial" target="_blank">
-            <img src={twitter} />
-          </a>
-          <a href="https://www.instagram.com/wevoted" target="_blank">
-            <img src={instagram} />
-          </a>
+        <div className={styles.flexContainer}>
+          <form className={styles.newsletterForm}>
+            <label for="email">Join our mailing list</label>
+            <input type="email" name="email" placeholder="Enter your email address" />
+            <button type="submit">
+              <img src={button} />
+            </button>
+          </form>
+          <div className={styles.socialLinks}>
+            <a href="mailto:contact@wevoted.com">
+              <img src={email} />
+            </a>
+            <a href="https://www.facebook.com/wevotedofficial" target="_blank">
+              <img src={facebook} />
+            </a>
+            <a href="https://www.twitter.com/wevotedofficial" target="_blank">
+              <img src={twitter} />
+            </a>
+            <a href="https://www.instagram.com/wevoted" target="_blank">
+              <img src={instagram} />
+            </a>
+          </div>
         </div>
       </InnerContainer>
     </footer>
