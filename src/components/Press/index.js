cb2ede265160b26cf6e0e0d9e121a68815a5da14
Andy Gillette <andy.gillette@gmail.com>
2019-07-18 17:21:53 -0700

    upworthy logo wip

--- src/components/Press/index.js
+++ src/components/Press/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './press.module.scss';
 import InnerContainer from '../../elements/InnerContainer';
+import upworthyLogo from '../../images/upworthy.png';
 
 const Press = () => {
   return (
     <div className={styles.pressContainer}>
       <InnerContainer>
         <blockquote>"It's like Newman's Own but for pissed off progressives."</blockquote>
+        <img className={styles.upworthyLogo} src={upworthyLogo} />
       </InnerContainer>
     </div>
   );
