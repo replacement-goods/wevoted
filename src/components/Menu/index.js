58b6b7527aece38670f090c76146e9a5c6b4f0d7
Andy Gillette <andy.gillette@gmail.com>
2019-07-23 22:42:34 -0700

    contact form

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -5,26 +5,73 @@ import styles from './menu.module.scss';
 
 const Menu = () => {
   const [open, setOpen] = useState(false);
+  const [contactVisible, setContactVisible] = useState(false);
 
   return (
     <div className={classnames(styles.menu, open && styles.menuOpen)}>
       <div className={styles.persistentNav}>
-        <a href="https://www.indiegogo.com/projects/these-home-goods-defeat-right-wing-politicians/" target="_blank">Support our IndieGoGo Campaign</a>
-        <a href="mailto:contact@wevoted.com">Contact Us</a>
+        <a
+          href="https://www.indiegogo.com/projects/these-home-goods-defeat-right-wing-politicians/"
+          target="_blank"
+        >
+          Support our IndieGoGo Campaign
+        </a>
+        <a
+          onClick={() => {
+            setOpen(true);
+            setContactVisible(true);
+          }}
+        >
+          Contact Us
+        </a>
       </div>
-      <a className={styles.navToggle} onClick={() => setOpen(!open)}><span></span></a>
+      <a
+        className={styles.navToggle}
+        onClick={() => {
+          setOpen(!open);
+          if (!open) {
+            setContactVisible(false);
+          }
+        }}
+      >
+        <span></span>
+      </a>
       <div className={styles.overlay}>
-        <nav className={styles.mainNav}>
-          <a href="mailto:contact@wevoted.com">Contact Us</a>
-          <a href="https://www.facebook.com/wevotedofficial" target="_blank">Facebook</a>
-          <a href="https://www.twitter.com/wevotedofficial" target="_blank">Twitter</a>
-          <a href="https://www.instagram.com/wevoted" target="_blank">Instagram</a>
+        <nav className={classnames(styles.mainNav, !contactVisible && styles.visible)}>
+          <a
+            onClick={() => {
+              setContactVisible(true);
+            }}
+          >
+            Contact Us
+          </a>
+          <a href="https://www.facebook.com/wevotedofficial" target="_blank">
+            Facebook
+          </a>
+          <a href="https://www.twitter.com/wevotedofficial" target="_blank">
+            Twitter
+          </a>
+          <a href="https://www.instagram.com/wevoted" target="_blank">
+            Instagram
+          </a>
           <Link to="/terms/">Terms and Conditions</Link>
         </nav>
-        <form className={styles.contactForm} method="post" action="#" data-netlify="true" name="contact">
+        <form
+          className={classnames(styles.contactForm, contactVisible && styles.visible)}
+          method="post"
+          action="#"
+          data-netlify="true"
+          name="contact"
+        >
           <input type="hidden" name="form-name" value="contact" />
-          <input type="text" name="name" id="name" required />
-          <input type="text" name="email" id="email" required />
+          <input type="text" placeholder="name" name="name" id="name" required />
+          <input
+            type="text"
+            placeholder="email address"
+            name="email"
+            id="email"
+            required
+          />
           <textarea name="message" id="message" rows="4" required />
           <input type="submit" value="Send Message" />
         </form>
