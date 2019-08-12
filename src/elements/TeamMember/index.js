2a3e9a9aee955523dcec16fe142aed2a2966f8d0
Andy Gillette <andy.gillette@gmail.com>
2019-08-12 12:45:49 -0700

    team page updates

--- src/elements/TeamMember/index.js
+++ src/elements/TeamMember/index.js
@@ -4,12 +4,16 @@ import styles from './teamMember.module.scss';
 const TeamMember = ({ name, title, children: { headshot, bio } }) => {
   return (
     <div className={styles.teamMember}>
-      <div className={styles.imageColumn}>
-        {headshot}
+      <div className={styles.flexContainer}>
+        <div className={styles.imageColumn}>
+          {headshot}
+        </div>
+        <div className={styles.textColumn}>
+          <h3>{name}</h3>
+          <h4>{title}</h4>
+        </div>
       </div>
-      <div className={styles.textColumn}>
-        <h3>{name}</h3>
-        <h4>{title}</h4>
+      <div className={styles.bioText}>
         {bio}
       </div>
     </div>
