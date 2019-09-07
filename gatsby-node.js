921fafdb017009172b4a6ce5acc5c9ab1b27d92e
Andy Gillette <andy.gillette@gmail.com>
2019-09-06 17:14:22 -0700

    redirect launch

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,6 +7,16 @@
 // You can delete this file if you're not using it
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createRedirect } = actions
-  createRedirect({ fromPath: '/fundraiser-preview', toPath: 'https://www.indiegogo.com/project/preview/407c08d9?utm_source=website&utm_medium=email&utm_campaign=fundraiser_preview', isPermanent: true })
-}
+  const { createRedirect } = actions;
+  createRedirect({
+    fromPath: '/launch',
+    toPath:
+      'https://www.indiegogo.com/projects/these-home-goods-defeat-right-wing-politicians/',
+  });
+  createRedirect({
+    fromPath: '/fundraiser-preview',
+    toPath:
+      'https://www.indiegogo.com/project/preview/407c08d9?utm_source=website&utm_medium=email&utm_campaign=fundraiser_preview',
+    isPermanent: true,
+  });
+};
