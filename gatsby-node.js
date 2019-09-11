eceb04c4157e3efd40483e023d0053843fc4da9e
Andy Gillette <andy.gillette@gmail.com>
2019-09-11 10:27:08 -0700

    bunch of redirects

--- gatsby-node.js
+++ gatsby-node.js
@@ -19,4 +19,34 @@ exports.createPages = ({ graphql, actions }) => {
       'https://www.indiegogo.com/project/preview/407c08d9?utm_source=website&utm_medium=email&utm_campaign=fundraiser_preview',
     isPermanent: true,
   });
+  createRedirect({
+    fromPath: '/aplaunch',
+    toPath: 'https://www.indiegogo.com/projects/we-voted-our-soaps-defeat-right-wing-politicians/?utm_source=twitter&utm_medium=referral&utm_campaign=AdamParkhomenko_launch_video',
+    isPermanent: true,
+  });
+  createRedirect({
+    fromPath: '/twlaunch',
+    toPath: 'https://www.indiegogo.com/projects/we-voted-our-soaps-defeat-right-wing-politicians/?utm_source=twitter&utm_medium=referral&utm_campaign=wevotedofficial_launch_video',
+    isPermanent: true,
+  })
+  createRedirect({
+    fromPath: '/twitter',
+    toPath: 'https://www.indiegogo.com/projects/we-voted-our-soaps-defeat-right-wing-politicians/?utm_source=twitter&utm_medium=referral&utm_campaign=launch_video',
+    isPermanent: true,
+  });
+  createRedirect({
+    fromPath: '/fblaunch',
+    toPath: 'https://www.indiegogo.com/projects/we-voted-our-soaps-defeat-right-wing-politicians/?utm_source=facebook&utm_medium=referral&utm_campaign=wevotedofficial_launch_video',
+    isPermanent: true,
+  });
+  createRedirect({
+    fromPath: '/facebook',
+    toPath: 'https://www.indiegogo.com/projects/we-voted-our-soaps-defeat-right-wing-politicians/?utm_source=facebook&utm_medium=referral&utm_campaign=launch_video',
+    isPermanent: true,
+  });
+  createRedirect({
+    fromPath: '/ytlaunch',
+    toPath: 'https://www.indiegogo.com/projects/we-voted-our-soaps-defeat-right-wing-politicians/?utm_source=youtube&utm_medium=referral&utm_campaign=launch_video',
+    isPermanent: true,
+  });
 };
