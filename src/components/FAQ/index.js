e0e4f10568ed0e61127e7e18f984f95b5243e5e2
Andy Gillette <andy.gillette@gmail.com>
2019-09-12 08:21:31 -0700

    faq updates

--- src/components/FAQ/index.js
+++ src/components/FAQ/index.js
@@ -11,36 +11,81 @@ const FAQ = () => {
           <div className={styles.question}>
             <h3>How do we know where the money is going?</h3>
             <p>
-              We're committed to 100% transparency and we'll post everything on our
+              We’re committed to 100% transparency and we’ll post everything on our
               website. But basically your money will be going to run ads targeting
               right-wing politicians running for office.
+            </p>
+            <p>
+              In the long run we have a bigger dream for We Voted®. Running attack ads
+              against Republicans can only do so much. The hard truth is that for the last
+              30 years the Right has taken huge amounts of money from big corporations and
+              built an elaborate network of organizations like the Federalist Society, the
+              Club for Growth and the American Legal Exchange Council (ALEC) which are a
+              big part of where its power comes from. Eventually we hope We Voted® can
+              help raise money to fund these kinds of organizations on the Left.
             </p>
           </div>
           <div className={styles.question}>
             <h3>Which candidates will you target?</h3>
             <p>
-              We'll make decisions opportunistically with an eye towards maximizing the
-              impact at both the national and state levels.
+              We’ll make decisions opportunistically while maximizing the impact at both
+              the national and state levels. Right now we believe investing in purple
+              state Senate elections is the smartest move. But that may change in the
+              coming months and certainly in the coming years.
+            </p>
+          </div>
+          <div className={styles.question}>
+            <h3>What do you guys know about making high quality products?</h3>
+            <p>
+              This is going to really shock you, but most of the companies making soaps
+              are using formulations that have been around for a hundred years. The
+              innovative stuff happening in the world of soaps and cleaners has to do with
+              using plant extracts like thymol which is an extract from the herb thyme.
+              Anyway, we’re synthesizing all-natural old-school formulations with some of
+              these newer plant-based additives to make some great products. They are
+              simple, high quality and all-natural.
+            </p>
+          </div>
+          <div className={styles.question}>
+            <h3>How is this legal?</h3>
+            <p>
+              Great question. First of all, it shouldn’t be. But it is. We are a
+              for-profit California C-Corporation. Totally legal. As a company we are
+              committed to pouring half of our profits into our super PAC. Totally legal.
+              And that super PAC will run targeted political ads aimed at throwing corrupt
+              right-wing monsters out of office. Totally legal.
+            </p>
+            <p>
+              The funny part is that we’re doing exactly what pretty much every other
+              corporation in America is doing… We’re just being transparent about it.
             </p>
           </div>
           <div className={styles.question}>
             <h3>Why did you choose to sell these kinds of products?</h3>
             <p>
-              The truth is that we chose these kinds of products because they're fairly
-              easy to manufacture. Down the road we hope to expand this business into many
-              other product categories.
+              The truth is that we chose these kinds of products because they’re fairly
+              easy to manufacture at a high quality. Down the road we hope to expand this
+              business into many other product categories. Right now you are more than
+              likely subscribed to a mobile phone carrier that gives HUGE money to
+              Republicans. We believe progressives should have an alternative.
             </p>
           </div>
           <div className={styles.question}>
             <h3>You're selling products in plastic bottles, wtf?</h3>
             <p>
-              It's true. But hear us out. It's pretty hard to start a project like this
-              AND be low impact. To be honest the supply chains aren't really there yet to
-              provide eco-friendly packaging for these kinds of products at a cost that
-              would make this project viable. We're working on better solutions. First of
-              all eco-plastics are super promising but many of the supposedly recyclable
-              plastics aren't as they seem. We're exploring lot's of alternatives and all
-              we can say is that we'll get better.
+              It’s true. Sadly the supply chains aren’t really there yet to provide
+              eco-friendly packaging for these kinds of products at a cost that would make
+              this project viable.
+            </p>
+            <p>
+              We’re working on better solutions. First of all eco-plastics are promising
+              but many of the supposedly recyclable plastics aren’t as they seem. We’re
+              exploring lots of alternatives and all we can say is that we’ll get better.
+            </p>
+            <p>
+              More than likely we will get to a place where we are providing our customers
+              with refillable bottles and sending out our liquid products in milk cartons
+              made of paper. But that’ll happen next year.
             </p>
           </div>
           <div className={styles.question}>
@@ -49,20 +94,19 @@ const FAQ = () => {
               make things worse?
             </h3>
             <p>
-              First of all, it's hard to imagine things getting much worse. Second of all,
-              all we're doing is fighting fire with fire. Corporate America already
-              bankrolls the Right. All we're doing is trying to help fight back. And
-              further, if the courts overturned Citizens United and made what we're doing
-              illegal we would shut We Voted® down and dance in the streets.
+              First of all, it’s hard to imagine things getting much worse. Second of all,
+              all we’re doing is fighting fire with fire. Corporate America already
+              bankrolls the Right. All we’re doing is trying to help fight back. And
+              further, if the courts overturned Citizens United and made what we’re doing
+              illegal, we would shut We Voted® down and dance in the streets.
             </p>
           </div>
           <div className={styles.question}>
             <h3>Who are you? How can we trust you?</h3>
             <p>
               Fair questions. We are just ordinary pissed off progressives who decided to
-              do something. You can follow everything we're doing on our social media
-              pages. We'll try to keep everything posted. If you have more questions, ask
-              us in the comments and we'll answer.
+              do something. You can follow everything we’re doing on our social media
+              pages. If you have more questions, ask us in the comments and we’ll answer.
             </p>
           </div>
           <div className={styles.question}>
@@ -71,50 +115,39 @@ const FAQ = () => {
               that you claim?
             </h3>
             <p>
-              Yes and no. We did a lot of research. First off, Dr. Bronner's (as best we
-              can tell) walks on water. But generally all the bigger ones all dirty. Even
-              if they claim they're not if you dig into their lobbying efforts and their
-              trade organizations they're filthy. But it's not just the companies
-              themselves. Their supply chains are dirty. The shipping companies give HUGE
-              money to right wing causes. And don't get us started on the packaging
-              companies. They might be the worst of all.
+              Yes and no. We did a lot of research. First off, Dr. Bronner’s (as best we
+              can tell) walks on water. But generally all the bigger ones are dirty. Even
+              if they claim they’re not they are laundering the money through lobbying
+              firms and trade organizations that do the dirty work.
+            </p>
+            <p>
+              Their PR firms will say things like: We give money to leaders from both
+              parties to make sure that the interests of our shareholders blah blah blah.
+              It’s bullshit. The only correct amount of money to give to politicians in
+              bed with the gun lobby is ZERO. The only correct amount of money to give to
+              politicians who are ok with separating migrant kids from their mothers is
+              ZERO. I’m getting pissed off just writing this.
             </p>
           </div>
           <div className={styles.question}>
             <h3>Isn't this just a drop in the bucket?</h3>
             <p>
-              First of all, it doesn't have to be. If we can convince a million people to
+              First of all, it doesn’t have to be. If we can convince a million people to
               join in on this it will create an earthquake in American politics. And
-              that's obviously our goal. But it sure would be nice if the corporations
+              that’s obviously our goal. But it sure would be nice if the corporations
               that are underwriting all of these terrible politicians got shamed into
               turning off the faucet. We have no illusions about the scale of this
-              problem. But it's too important not to try.
+              problem. But it’s too important not to try.
             </p>
           </div>
           <div className={styles.question}>
-            <h3>How is this legal?</h3>
+            <h3>What's the deal with these products?</h3>
             <p>
-              Great question. First of all, it shouldn't be. But it is. We are a
-              for-profit California C Corporation. Totally legal. As a company we are
-              committed to pouring half of our profits into our super PAC. Totally legal.
-              And that super PAC runs targeted political ads aimed at throwing corrupt
-              right-wing monsters out of office. Totally legal. The funny part is that
-              we're doing exactly what pretty much every other corporation in America is
-              doing... We're just being transparent about it.
-            </p>
-          </div>
-          <div className={styles.question}>
-            <h3>What the heck do you guys know about making high quality products?</h3>
-            <p>
-              This is going to really shock you, but most of the companies making soaps
-              are using formulations that have been around for a hundred years. For awhile
-              companies were using synthetic compounds and weird chemicals in their
-              products but that's mostly stopped. The innovative stuff happening in the
-              world of soaps and cleaners has to do with using plant extracts like thymol
-              which is an extract from the herb thyme. Thymol is used in place of harsh
-              alcohols in things like wipes. Anyway, we're synthesizing all natural old
-              school formulations with some of these newer plant based additives to make
-              some great products. They are simple, high quality and all natural.
+              We are committed to manufacturing and selling safe all-natural products.
+              Currently we are making our products in Illinois at a small family run
+              facility. As we expand our product line we are committed to making goods our
+              customers will love. We will always work our hardest to exceed your
+              expectations.
             </p>
           </div>
         </article>
